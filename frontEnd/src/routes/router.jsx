d9b6a6dd3cee42d0dabdf5b274ccfc47de7cd5e9
PatiphatRattanosot <654259017@webmail.npru.ac.th>
2024-09-28 03:19:57 +0000

    update router

--- frontEnd/src/routes/router.jsx
+++ frontEnd/src/routes/router.jsx
@@ -1,10 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
+import Layout from "../components/Layout";
 import HomePage from "../pages/HomePage";
 import RegisterPage from "../pages/RegisterPage";
 import LoginPage from "../pages/LoginPage";
-import Layout from "../components/Layout";
-import ProfilePage from '../pages/ProfilePage'
-import EditProfilePage from "../pages/EditProfilePage";
+import ProfilePage from "../pages/personnel/ProfilePage";
+import EditProfilePage from "../pages/personnel/EditProfilePage";
+import AddNewsPage from "../pages/news/AddNewsPage";
+import EditNewsPage from "../pages/news/EditNewsPage";
+import DepartmentPage from "../pages/department/dashbord/DepartmentPage";
+import EditDepartmentPage from "../pages/department/EditDepartmentPage";
+import PersonnelPage from "../pages/personnel/dashboard/index";
+import EditPersonnelPage from "../pages/personnel/EditProfilePage";
+import NotAllow from "../components/checkpPermissions/NotAllow";
+import AdminOnly from "../components/checkpPermissions/AdminOnly";
+import AdminOrTeacher from "../components/checkpPermissions/AdminOrTeacher";
+
+
 const route = createBrowserRouter([
     {
         path: "/",
@@ -25,6 +36,30 @@ const route = createBrowserRouter([
             }, {
                 path: "/profile/edit",
                 element: <EditProfilePage/>
+            }, {
+                path: "/profile/edit/:id",
+                element: <AdminOnly><EditProfilePage/></AdminOnly>
+            }, {
+                path: "/news/add",
+                element:<AdminOrTeacher><AddNewsPage/></AdminOrTeacher> 
+            }, {
+                path: "/news/edit/:id",
+                element: <AdminOrTeacher><EditNewsPage/></AdminOrTeacher>
+            },{
+                path:"/dashboard/department",
+                element: <AdminOnly><DepartmentPage/></AdminOnly>
+            },{
+                path:"/dashboard/department/edit/:id",
+                element: <AdminOnly><EditDepartmentPage/></AdminOnly>
+            },{
+                path:"/dashboard/personnel",
+                element:<PersonnelPage/>
+            },{
+                path:"/dashboard/personnel/edit/:id",
+                element:<EditPersonnelPage/>
+            },{
+                path:"/notallow",
+                element:<NotAllow/>
             }
         ]
     }
